8065a0a2daf2b2274aee96e9a7394b9de00b187b
gabo543 <gabrielnicolashachim@gmail.com>
2025-12-22 17:27:53 +0200

    Add files via upload

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1888,10 +1888,9 @@ return Column(
 
   Widget? _buildFab() {
     if (vista == Vista.lista || vista == Vista.favoritos || vista == Vista.borrar) {
-      // Solo icono (sin texto "Inicio")
+      // Solo icono (sin texto)
       return FloatingActionButton(
         onPressed: () => setState(() => vista = Vista.inicio),
-        tooltip: 'Inicio',
         child: const Icon(Icons.home),
       );
     }
